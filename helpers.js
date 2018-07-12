01382e10061428231efd67168cc24aeb285b06c2
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-07-12 17:47:35 +0300

    replaced for on for of in helpers.js

--- helpers.js
+++ helpers.js
@@ -1,30 +1,31 @@
 function checkIsNumbers(numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        if (Number.isNaN(numbers[i])) {
+    for (let number of numbers) {
+        if (!Number.isInteger(number)) {
             throw 'One of the variables is not number';
         }
     }
 }
-function checkCountArgs(args){
+
+function checkCountArgs(args) {
     if (args.length === 0) {
         throw 'Count parameters equally 0';
     }
 }
-function checkIsArray(arr){
-    if (!Array.isArray(arr))
-    {
+
+function checkIsArray(arr) {
+    if (!Array.isArray(arr)) {
         throw 'This is not array';
     }
 }
-function checkIsObject(obj){
-    if (typeof obj !== 'object')
-    {
+
+function checkIsObject(obj) {
+    if (typeof obj !== 'object') {
         throw 'This is not object';
     }
 }
-function checkIsFunction(fn){
-    if (typeof fn !== 'function')
-    {
+
+function checkIsFunction(fn) {
+    if (typeof fn !== 'function') {
         throw 'This is not function';
     }
 }
